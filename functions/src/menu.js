f81f04728ef86b1b7807fb198583b478fa79a546
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2020-01-16 09:41:48 +0200

    Added some new menus to the remove list

--- functions/src/menu.js
+++ functions/src/menu.js
@@ -1,78 +1,76 @@
-import axios from "axios";
-import dayjs from "dayjs";
+import axios from "axios"
+import dayjs from "dayjs"
 
-import { botUrl } from "./index";
+import { botUrl } from "./index"
 
 export const sendMenu = async chat_id => {
-  try {
-    const menu = menuToString(
-      (await getMenu(dayjs().format("YYYY-MM-DD"))).data
-    );
+    try {
+        const menu = menuToString((await getMenu(dayjs().format("YYYY-MM-DD"))).data)
 
-    await axios.post(botUrl + "/sendMessage", {
-      chat_id,
-      text: menu,
-      parse_mode: "HTML"
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
+        await axios.post(botUrl + "/sendMessage", {
+            chat_id,
+            text: menu,
+            parse_mode: "HTML"
+        })
+    } catch (e) {
+        console.error(e)
+    }
+}
 
 export const getMenu = async date => {
-  const url = `https://api.ruoka.xyz/${date}`;
+    const url = `https://api.ruoka.xyz/${date}`
 
-  try {
-    const results = await axios.get(url);
-    return results;
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        const results = await axios.get(url)
+        return results
+    } catch (e) {
+        console.error(e)
+    }
 
-  return {};
-};
+    return {}
+}
 
 export const menuToString = menu => {
-  let menuString = "";
+    let menuString = ""
 
-  const remove = [
-    // Menus
-    "Street food",
-    "Fusion Kitchen",
+    const remove = [
+        // Menus
+        "Fusion Kitchen",
 
-    // Meal
-    "Iltaruoka (16.00 - 18.00)",
-    "A´la carte (10.30 - 14.00)",
-    "special (10.30 - 14.00)",
-    "leipäateria",
-    "salaattiaterian proteiiniosa",
-    "Warm Salad",
-    "Grill",
-    "Pasta",
-    "Kasviskeitto",
-    "kasviskeitto"
-  ];
+        // Meal
+        "Lämmin salaatti",
+        "Wok / Pasta / Burger / Grill ",
+        "Erikoislounas",
+        "Iltaruoka (16.00 - 18.00)",
+        "A´la carte (10.30 - 14.00)",
+        "special (10.30 - 14.00)",
+        "leipäateria",
+        "salaattiaterian proteiiniosa",
+        "Warm Salad",
+        "Grill",
+        "Pasta",
+        "Kasviskeitto",
+        "kasviskeitto"
+    ]
 
-  menu.restaurants.forEach(restaurant => {
-    menuString += `<b>${restaurant.name}</b>: `;
-    restaurant.menus.forEach(menu => {
-      if (!remove.includes(menu.name)) {
-        menuString += `\t<i>${menu.name}</i>\n`;
-        menu.meals.forEach(meal => {
-          if (!remove.includes(meal.name)) {
-            menuString += `\t\t<b>${meal.name} - </b>`;
-            meal.contents.forEach((food, index) => {
-              menuString += `<code>${food.name}${
-                index === meal.contents.length - 1 ? "" : ", "
-              }</code>`;
-            });
-            menuString += "\n";
-          }
-        });
-      }
-    });
-    menuString += "\n\n\n";
-  });
+    menu.restaurants.forEach(restaurant => {
+        menuString += `<b>${restaurant.name}</b>: `
+        restaurant.menus.forEach(menu => {
+            if (!remove.includes(menu.name)) {
+                menuString += `\t<i>${menu.name}</i>\n`
+                menu.meals.forEach(meal => {
+                    if (!remove.includes(meal.name)) {
+                        menuString += `\t\t<b>${meal.name} - </b>`
+                        meal.contents.forEach((food, index) => {
+                            menuString += `<code>${food.name}${index === meal.contents.length - 1 ? "" : ", "}</code>`
+                        })
+                        menuString += "\n"
+                    }
+                })
+            }
+        })
+        menuString += "\n\n\n"
+    })
 
-  return menuString;
-};
+    return menuString
+}
